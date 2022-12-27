845546c98db5c92143f02be2f26607fc4e9a295e
Gabriel Audette <201955130@edu.clg.qc.ca>
2022-12-27 05:27:16 -0500

    Ajout tout contenu, fonctions, verifications, etc. push majeur

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # AUD_PFI
 
-
 Projet Final Integration
 Collège Lionel-Groulx
 
 Date : 12-19-2022
 
 Programmation Web 1
+
+
+Dans ce projet, j'écris un serveur Web qui va valider un formulaire d'identification de connexion au serveur qui va vérifier les données recues selon un fichier externe.
+
+J'utilise le package JSON NodeMon, que j'exclus dans la publication Github de mon projet
+
+Joyeux Temps des Fêtes Mr. Pilon!
--- index.js
+++ index.js
@@ -2,107 +2,176 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const PORT = process.env.PORT || 8000;
+const usager = require("./liste_usagers");
+const SUCCESS = 200;
+const UNAUTHORIZED = 401;
+const UNEXISTING = 404;
+const ERRORSERVER = 500;
+
+// Dans ce projet, j'ai une page Login qui prends un user & un pass, qui redirige a une des 3 pages HTML selon le rôle donné de l'utilisateur donné, que je passe en param POST / GET avec mon formulaire
+// Quand j'exécute l'action du formulaire vérification si le user & password match la liste reçu
+// Redirection a la page du role selon la liste
 
 // Création & appel du serveur
-let serveur = http.createServer((requete, reponse) => {
-  if (requete.url === "/") {
-    let fileName = path.join(__dirname, "", "login.html");
-    fournirPageWeb(fileName, reponse);
-  } else {
-    let fileName = path.join(__dirname, "", requete.url);
-    fournirPageWeb(fileName, reponse);
-  }
-});
-
-// Appel du serveur
-serveur.listen(PORT, () =>
-  console.log(`Le serveur est démarré sur le port :${PORT}`)
-);
-
-function test() {
-  alert(`123`);
-}
+http
+  .createServer((requete, reponse) => {
+    if (requete.url.substring(0, 11) == "/acess.html") {
+      // preventDefault();
+      traiterPostBack(requete, reponse);
+    } else if (requete.url === "/" || requete.url === "/login.html") {
+      // let fileName = path.join(__dirname, "pagesWeb", "login.html");
+      fournirPageWeb("login.html", reponse);
+    } else if (requete.url === "/login_get.html") {
+      fournirPageWeb("login_get.html", reponse);
+    } else {
+      // let fileName = path.join(__dirname, "pagesWeb", requete.url);
+      fournirPageWeb("", reponse);
+    }
+  })
+  .listen(PORT, () =>
+    console.log(`Le serveur est démarré sur le port :${PORT}`)
+  );
+//
 function fournirPageWeb(fileName, reponse) {
-  console.log(fileName);
-  fs.readFile(fileName, (err, contenu) => {
+  let nomFichier = path.join(__dirname, "pagesWeb", fileName);
+  let fileType = typeContenu(fileName);
+
+  fs.readFile(nomFichier, (err, contenu) => {
     if (err) {
       if (err.code === "ENOENT") {
         // Fichier non trouvé
-        reponse.writeHead(404, { "Content-Type": "text/html" });
+        reponse.writeHead(UNEXISTING, { "Content-Type": "text/html" });
         reponse.write("<h1>Page Introuvable 404</h1>");
         reponse.end();
       } else {
-        reponse.writeHead(500);
+        reponse.writeHead(ERRORSERVER);
         reponse.write(`Erreur du serveur code : ${err.code}`);
         reponse.end();
       }
     } else {
-      reponse.writeHead(200, { "Content-Type": "text/html" });
+      reponse.writeHead(SUCCESS, { "Content-Type": fileType });
       reponse.write(contenu);
       reponse.end();
     }
   });
 }
-// Changement page Web
-// function changerPageWeb(fileName, reponse) {
-//   console.log(fileName);
-//   fs.readFile(fileName, (err, contenu) => {
-//     if (err) {
-//       if (err.code === "ENOENT") {
-//         // Fichier non trouvé
-//         reponse.writeHead(404, { "Content-Type": "text/html" });
-//         reponse.write("<h1>Page Introuvable 404</h1>");
-//         reponse.end();
-//       } else {
-//         reponse.writeHead(500);
-//         reponse.write(`Erreur du serveur code : ${err.code}`);
-//         reponse.end();
-//       }
-//     } else {
-//       reponse.writeHead(200, { "Content-Type": "text/html" });
-//       reponse.write(contenu);
-//       reponse.end();
-//     }
-//   });
-// }
-
-function extraction_donnes() {
-  const user = document.getElementsByClassName("user").value;
-  console.log(user);
-  const pass = document.getElementsByClassName("pass");
+// Traite la demande de form en post, si le demande form positive, renvoie les params d'url dans le 3eme param en appelant la fonction traiterRequete
+function traiterPOST(requete, reponse) {
+  let postData = "";
+  requete.on("data", (donnee) => {
+    postData = postData + donnee;
+  });
+  requete.on("end", () => {
+    // Si rien recu dans l'url
+    if (postData === "") {
+      console.log(`Rien recu`);
+    } else {
+      const parametres = new URLSearchParams(postData);
+      traiterRequete(requete, reponse, parametres);
+    }
+  });
 }
-// extrait de code
-// if
-// reponse.write("<h1>Page Introuvable...</h1>\n");
-// fs.readFile(
-//   path.join(__dirname, "pagesWeb", "index.html"),
-//   (err, contenu) => {
-//     if (err) throw err;
-//     reponse.writeHead(200, { "Content-Type": "text/html" });
-//     reponse.write(contenu);
-//     reponse.end();
-//   }
-// );
-// else
-// reponse.end();
-// let serveur = http.createServer((requete, reponse) => {
+// Fonction TraiterGET (Va chercher les params de user & password avec le substring)
+function traiterGET(requete, reponse) {
+  const parametres = new URLSearchParams(requete.url.substring(12));
+  traiterRequete(requete, reponse, parametres);
+}
+// Traite la requête selon la méthode
+function traiterPostBack(requete, reponse) {
+  console.log(requete.method);
+  if (requete.method === "POST") {
+    // Ici on traite un POST
+    traiterPOST(requete, reponse);
+  } else {
+    // Ici ça veut dire que la méthode est GET
+    traiterGET(requete, reponse);
+  }
+}
+// Fonction traiter Requete qui va être appelé par les fonctions POST / GET , passé en 3eme param les user / pass
+function traiterRequete(requete, reponse, parametres) {
+  const user = parametres.get("user");
+  const pass = parametres.get("pass");
+  // déclaration de la variable qui va chercher le data du usager
+  let userTrouver = usager.find((data) => data.login === user);
+  if (userTrouver) {
+    if (userTrouver.pwd === pass) {
+      if (userTrouver.acces === "admin") {
+        changementContenuPageWeb("pageAdmin.html", reponse, userTrouver);
+      } else if (userTrouver.acces === "normal") {
+        changementContenuPageWeb("pageUsager.html", reponse, userTrouver);
+      } else if (userTrouver.acces === "restreint") {
+        changementContenuPageWeb("pageRestreinte.html", reponse, userTrouver);
+      }
+    } else {
+      console.log(`Mot de passe invalide`);
+      userTrouver = `Mot de passe invalide`;
+      changementContenuPageWeb("", reponse, userTrouver);
+    }
+  } else {
+    changementContenuPageWeb("", reponse, user);
+  }
+}
+// Fonction Change le contenu de la page Web selon l'appel depuis la fonction traiter rêquete
+function changementContenuPageWeb(fileName, reponse, userTrouver) {
+  let nomFichier = path.join(__dirname, "pagesWeb", fileName);
+  let fileType = typeContenu(fileName);
 
-//   if (requete.url === "/") {
-//     fs.readFile(
-//       path.join(__dirname, "pagesWeb", "index.html"),
-//       (err, contenu) => {
-//         if (err) throw err;
-//         reponse.writeHead(200, { "Content-Type": "text/html" });
-//         reponse.write(contenu);
-//         reponse.end();
-//       }
-//     );
-//   } else {
-//     reponse.write("<h1>Page Introuvable...</h1>\n");
-//     reponse.end();
-//   }
-// });
-
-// .listen(PORT, () => {
-//   console.log(`Le service web est demarré`);
-// });
+  fs.readFile(nomFichier, (err, contenu) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // Fichier non trouvé
+        reponse.writeHead(UNEXISTING, { "Content-Type": "text/html" });
+        reponse.write("<h1>Page Introuvable 404</h1>");
+        reponse.end();
+      } else {
+        if (userTrouver === `Mot de passe invalide`) {
+          reponse.writeHead(UNAUTHORIZED, { "Content-Type": "text/html" });
+          reponse.write("<h1>Page Non authaurisee 404</h1>");
+          reponse.write("<h1>Le mot de passe invalide</h1>");
+          reponse.end();
+        } else if (err.code === ERRORSERVER) {
+          reponse.writeHead(ERRORSERVER);
+          reponse.write(`Erreur du serveur code : ${err.code}`);
+          reponse.end();
+        } else {
+          reponse.writeHead(UNAUTHORIZED, { "Content-Type": "text/html" });
+          reponse.write("<h1>Page Non Authorisee 404</h1>");
+          reponse.write(
+            `<h1>Le nom d'utilisateur est non valide ${userTrouver}</h1>`
+          );
+          reponse.end();
+        }
+      }
+      // Si pas d'erreur, retourner les informations par les bonnes dans la page
+    } else {
+      reponse.writeHead(SUCCESS, { "Content-Type": fileType });
+      reponse.write(contenu.replace(/_nom_nom/g, userTrouver.nom));
+      reponse.write(contenu.replace(/_login_login/g, userTrouver.login));
+      reponse.end();
+    }
+  });
+}
+// Fonction qui vérifie le type de contenu
+function typeContenu(nomFichier) {
+  let extFichier = path.extname(nomFichier).toLowerCase();
+  // vérifie l'extFichier pour retourner le bon type de contenu
+  let typeDeContenu = "text/html";
+  switch (extFichier) {
+    case ".png":
+      typeDeContenu = "image/png";
+      break;
+    case ".jpg":
+      typeDeContenu = "image/jpg";
+      break;
+    case ".gif":
+      typeDeContenu = "image/gif";
+      break;
+    case ".js":
+      typeDeContenu = "text/javascript";
+      break;
+    case ".css":
+      typeDeContenu = "text/css";
+      break;
+  }
+  return typeDeContenu;
+}
